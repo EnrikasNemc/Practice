61de7c745cfd911bcbccec2e56f4e48f568c4bc6
EnrikasNemc <33262515+EnrikasNemc@users.noreply.github.com>
2017-11-17 03:28:34 +0200

    A bancrupt plane

--- Plane.php
+++ Plane.php
@@ -1,17 +1,54 @@
 <?php
 
-plane(250000, 0.3);
+function planefix($costs, $margin){
+	$FIRST_CLASS = 10;
+	$BUSINESS_CLASS = 30;
+	$ECONOMY_CLASS = 280;
 
-function plane($costs, $margin){
-	var FIRST_CLASS = 10;
-	var BUSINESS_CLASS = 30;
-	var ECONOMY_CLASS = 280;
+	$PRICE_FIRST = 10000;
+	$PRICE_BUSINESS = 4000;
+	$PRICE_ECONOMY = 500;
 
-	var PRICE_FIRST = 10000;
-	var PRICE_BUSINESS = 4000;
-	var PRICE_ECONOMY = 500;
+	$FILL_FIRST = 1;
+	$FILL_BUSINESS = 0.8;
+	$FILL_ECONOMY = 0.68;
 
-	var FILL_FIRST = 1;
-	var FILL_BUSINESS = 0.8;
-	var FILL_ECONOMY = 0.68;
+	// Without Margin
+	$A = $FIRST_CLASS*$PRICE_FIRST;
+	$B = $BUSINESS_CLASS*$PRICE_BUSINESS;
+	$C = $ECONOMY_CLASS*$PRICE_ECONOMY;
+	//With Margin
+	$a = ($FIRST_CLASS*$PRICE_FIRST)*$margin;
+	$b = ($BUSINESS_CLASS*$PRICE_BUSINESS)*$margin;
+	$c = ($ECONOMY_CLASS*$PRICE_ECONOMY)*$margin;
+	//Filled with people
+	$g = (($FIRST_CLASS*$PRICE_FIRST)*$FILL_FIRST)*$margin;
+	$h = (($BUSINESS_CLASS*$PRICE_BUSINESS)$FILL_BUSINESS)*$margin;
+	$i = (($ECONOMY_CLASS*$PRICE_ECONOMY)$FILL_ECONOMY)*$margin;
+
+
+	$d= $a+$b+$c;
+	$e= $A + $B + $C;
+	$E = $e - $costs;
+	$D = $d - $costs;
+	$F = $e - $d;
+
+	if($d<$costs){
+		Echo "The profits are in the negative </br>";
+	}
+
+
+
+
+	Echo "Possible profits before tax are $e </br>
+		  Possible profits from First Class: $a </br>
+		  Possible profits from Business Class: $b </br>
+		  Possible profits from Economy Class: $c </br>
+		  Total Possible Profits after tax: $D </br>
+		  Actual Profits from First Class: $g </br>
+		  Actual Profits from Business Class: $h </br>
+		  Actual Profits from Economy Class: $i </br>
+		  $E</br>
+		  $F";
+
 }
--- index.php
+++ index.php
@@ -54,6 +54,156 @@ function Logistics($a, $b, $c, $d){
 		  Pelnas: $B </br>";
 }
 
+function plane_2($costs, $margin){
+	$FIRST_CLASS = 10;
+	$BUSINESS_CLASS = 30;
+	$ECONOMY_CLASS = 280;
+
+	$PRICE_FIRST = 10000;
+	$PRICE_BUSINESS = 4000;
+	$PRICE_ECONOMY = 500;
+
+	$FILL_FIRST = 1;
+	$FILL_BUSINESS = 0.8;
+	$FILL_ECONOMY = 0.68;
+
+	// Without Margin
+	$A = $FIRST_CLASS*$PRICE_FIRST;
+	$B = $BUSINESS_CLASS*$PRICE_BUSINESS;
+	$C = $ECONOMY_CLASS*$PRICE_ECONOMY;
+	//With Margin
+	$a = ($FIRST_CLASS*$PRICE_FIRST)*$margin;
+	$b = ($BUSINESS_CLASS*$PRICE_BUSINESS)*$margin;
+	$c = ($ECONOMY_CLASS*$PRICE_ECONOMY)*$margin;
+
+
+	$d= $a+$b+$c;
+	$e= $A + $B + $C;
+	$E = $e - $costs;
+	$D = $d - $costs;
+
+
+	if($d<$costs){
+		Echo "The profits are in the negative </br>";
+	}
+
+
+	Echo "Possible profits before tax are $e </br>
+		  Possible profits from First Class: $a </br>
+		  Possible profits from Business Class: $b </br>
+		  Possible profits from Economy Class: $c </br>
+		  Total Possible Profits after tax: $D </br>
+		  $E";
+}
+
+function plane($costs, $margin){
+	$FIRST_CLASS = 10;
+	$BUSINESS_CLASS = 30;
+	$ECONOMY_CLASS = 280;
+
+	$PRICE_FIRST = 10000;
+	$PRICE_BUSINESS = 4000;
+	$PRICE_ECONOMY = 500;
+
+	$FILL_FIRST = 1;
+	$FILL_BUSINESS = 0.8;
+	$FILL_ECONOMY = 0.68;
+
+	// Without Margin
+	$A = $FIRST_CLASS*$PRICE_FIRST;
+	$B = $BUSINESS_CLASS*$PRICE_BUSINESS;
+	$C = $ECONOMY_CLASS*$PRICE_ECONOMY;
+	//With Margin
+	$a = ($FIRST_CLASS*$PRICE_FIRST)*$margin;
+	$b = ($BUSINESS_CLASS*$PRICE_BUSINESS)*$margin;
+	$c = ($ECONOMY_CLASS*$PRICE_ECONOMY)*$margin;
+
+
+	$d= $a+$b+$c;
+	$e= $A + $B + $C;
+	$E = $e - $costs;
+	$D = $d - $costs;
+	$F = $e - $d;
+
+	if($d<$costs){
+		Echo "The profits are in the negative </br>";
+	}
+
+
+	Echo "Possible income before tax are $e </br>
+		  Possible income from First Class: $a </br>
+		  Possible income from Business Class: $b </br>
+		  Possible income from Economy Class: $c </br>
+		  Total Possible Profits after tax: $D </br>
+		  $E</br>
+		  $F";
+
+}
+
+function planefix($costs, $margin){
+	$FIRST_CLASS = 10;
+	$BUSINESS_CLASS = 30;
+	$ECONOMY_CLASS = 280;
+
+	$PRICE_FIRST = 10000;
+	$PRICE_BUSINESS = 4000;
+	$PRICE_ECONOMY = 500;
+
+	$FILL_FIRST = 1;
+	$FILL_BUSINESS = 0.8;
+	$FILL_ECONOMY = 0.68;
+
+	// Without Margin
+	$A = $FIRST_CLASS*$PRICE_FIRST;
+	$B = $BUSINESS_CLASS*$PRICE_BUSINESS;
+	$C = $ECONOMY_CLASS*$PRICE_ECONOMY;
+	//With Margin
+	$a = ($FIRST_CLASS*$PRICE_FIRST)*$margin;
+	$b = ($BUSINESS_CLASS*$PRICE_BUSINESS)*$margin;
+	$c = ($ECONOMY_CLASS*$PRICE_ECONOMY)*$margin;
+	//Filled with people
+	$g = (($FIRST_CLASS*$PRICE_FIRST)*$FILL_FIRST)*$margin;
+	$h = (($BUSINESS_CLASS*$PRICE_BUSINESS)*$FILL_BUSINESS)*$margin;
+	$i = (($ECONOMY_CLASS*$PRICE_ECONOMY)*$FILL_ECONOMY)*$margin;
+
+
+	$d= $a+$b+$c;
+	$e= $A + $B + $C;
+	$E = $e - $costs;
+	$D = $d - $costs;
+	$F = $e - $d;
+	$G = $g + $h + $i;
+	//Passangers
+	$H = $ECONOMY_CLASS*$FILL_ECONOMY;
+	$I = $FIRST_CLASS*$FILL_FIRST;
+	$J = $BUSINESS_CLASS*$FILL_BUSINESS;
+	$k = $ECONOMY_CLASS + $BUSINESS_CLASS + $FIRST_CLASS;
+
+	$j = $H+$I+$J;
+	$K = $G - $costs;
+
+
+	if($d<$costs){
+		Echo "The profits are in the negative </br>";
+	}
+
+	Echo "Possible income before tax are $e </br>
+		  Possible income From First Class: $a </br>
+		  Possible income From Business Class: $b </br>
+		  Possible income From Economy Class: $c </br></br>
+		  Total Possible Profits after tax: $D </br>
+		  Income From First Class: $g </br>
+		  Income From Business Class: $h </br>
+		  Income From Economy Class: $i </br>
+		  Total  Income : $G </br>
+		  Profits: $K </br>
+		  Total Pasangers that flew: ";
+		  Echo floor($j);
+
+	Echo "</br> Out of Total possible: $k";
+
+}
+
 
 pitagor(10, 7);
 rectangleArea(10, 7);
@@ -63,3 +213,11 @@ trapezoidArea(5, 5, 4);
 PentagonArea(8);
 echo "</br>";
 Logistics(10, 8000, 400, 9000);
+echo "</br>";
+plane(250000, 0.7);
+echo "</br>";
+echo "</br>";
+plane_2(250000, 0.3);
+echo "</br>";
+echo "</br>";
+planefix(250000, 0.7);
